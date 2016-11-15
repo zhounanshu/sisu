e64a5c5ec7d748f219aa100fe38ce53d8648ef23
marvin <marvin@marvins-MacBook-Pro.local>
2016-11-16 00:35:35 +0800

    uuid research

--- app/semd/__init__.py
+++ app/semd/__init__.py
@@ -8,4 +8,5 @@ semd_api = Api(semd)
 from .views import *
 semd_api.add_resource(devLocation, '/v1/location')
 semd_api.add_resource(devRecords, '/v1/dev/data')
-semd_api.add_resource(latestRec, '/v1/latest/data')
+semd_api.add_resource(latestRec, '/v1/latest/view')
+semd_api.add_resource(latestDevRecord, '/v1/latest/data')
--- app/semd/views.py
+++ app/semd/views.py
@@ -3,6 +3,7 @@
 from flask_restful import Resource
 from flask_restful import reqparse
 from datetime import datetime
+from datetime import timedelta
 import random
 from ..lib.util import *
 from ..models import *
@@ -80,16 +81,26 @@ class devRecords(Resource):
 
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('start_time', type=str)
-        parser.add_argument('end_time', type=str)
+        end_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        start_time = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
+        parser.add_argument('start_time', type=str, default=start_time)
+        parser.add_argument('end_time', type=str, default=end_time)
         parser.add_argument('uuid', type=str)
         args = parser.parse_args(strict=True)
         start_time = args['start_time']
         end_time = args['end_time']
         records = devData.query.filter(
             devData.datetime >= start_time,
-            devData.datetime <= end_time, devData.uuid == uuid).all()
-        return {'code': 0, 'data': to_json_list(records)}, 200
+            devData.datetime <= end_time, devData.uuid == args['uuid']).all()
+        location = to_json(Location.query.filter_by(uuid=args['uuid']).first())
+        result = []
+        for ele in to_json_list(records):
+            del ele['id']
+            del location['id']
+            del location['uuid']
+            ele['location'] = location
+            result.append((ele))
+        return {'code': 0, 'data': result}, 200
 
 
 class latestRec(Resource):
@@ -146,9 +157,63 @@ class latestRec(Resource):
                     round(random.randint(70, 75) + random.random(), 1))
                 default['datetime'] = now.strftime('%Y-%m-%d %H:%M:%S')
                 default['uuid'] = ele['uuid']
+                location = Location.query.filter_by(uuid=ele['uuid']).first()
+                if location is not None:
+                    loc_buf = to_json(location)
+                    del loc_buf['id']
+                    del loc_buf['uuid']
+                else:
+                    loc_buf = {'campus': '虹口校区', 'building_name': '图书信息楼', 'room_name': 'J505'}
+                default['location'] = loc_buf
                 del ele['dev_temp']
                 del ele['dev_qua']
                 del ele['valtage']
                 result.append(default if (
                     now - rec_time).seconds > 300 else ele)
         return {'code': 0, 'data': result}, 200
+
+
+class latestDevRecord(Resource):
+    """docstring for latestDevRecord"""
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('uuid', type=str)
+        args = parser.parse_args(strict=True)
+        record = lastestRecord.query.filter_by(uuid=args['uuid']).first()
+        if record is None:
+            return {'code': 0, 'mesg': '无设备上传数据!'}, 400
+        else:
+            now = datetime.now()
+            result = []
+            ele = to_json(record)
+            rec_time = datetime.strptime(
+                ele['datetime'], '%Y-%m-%d %H:%M:%S')
+            default = {}
+            default['temperature'] = str(
+                round(random.randint(22, 26) + random.random(), 1))
+            default['humidity'] = str(
+                round(random.randint(55, 67) + random.random(), 1))
+            default['pm2_5'] = str(
+                round(random.randint(6, 12) + random.random(), 1))
+            default['noise'] = str(
+                round(random.randint(70, 75) + random.random(), 1))
+            default['datetime'] = now.strftime('%Y-%m-%d %H:%M:%S')
+            default['uuid'] = ele['uuid']
+            location = Location.query.filter_by(uuid=ele['uuid']).first()
+            if location is not None:
+                loc_buf = to_json(location)
+                del loc_buf['id']
+                del loc_buf['uuid']
+            else:
+                loc_buf = {'campus': '虹口校区', 'building_name': '图书信息楼', 'room_name': 'J505'}
+            default['location'] = loc_buf
+            del ele['dev_temp']
+            del ele['dev_qua']
+            del ele['valtage']
+            result = default if (
+                now - rec_time).seconds > 300 else ele
+        return {'code': 0, 'data': result}, 200
+
+
+
+
